6256e754ffecf7b3b9213374607c0966e29d70e0
4UR3L10 <aureliomartinezdros@outlook.com>
2019-05-06 22:30:11 -0400

    SolveIt

---
+++ src/williamapplication/WilliamApplication.java
@@ -0,0 +1,19 @@
+package williamapplication;
+
+/*
+    Make a program that use a formula to calculate 
+    the force based on the input from the user.
+    [Express the result and show the unit measure]
+*/
+
+public class WilliamApplication {
+
+    public static void main(String[] args) {
+        // Create variables below. 
+        
+        // Ask the user for input.
+        
+        // Show the result. (Example:"The value of the var is:" [*Unit measure include*])
+    }
+    
+}
