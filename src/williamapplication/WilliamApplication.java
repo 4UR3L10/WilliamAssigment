bc74881eff75cd2707455887db700fea1ef1112a
William A <w_aran69@yahoo.com>
2019-05-06 23:02:21 -0400

    IM DONEEEE

--- src/williamapplication/WilliamApplication.java
+++ src/williamapplication/WilliamApplication.java
@@ -5,15 +5,25 @@ package williamapplication;
     the force based on the input from the user.
     [Express the result and show the unit measure]
 */
+import java.util.Scanner;
 
 public class WilliamApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {        
         // Create variables below. 
-        
+        double force= 0.0;// Variable use for force
+        double m=0.0; // Variable use for mass
+        double a=0.0; // Variable use for acceleration              
         // Ask the user for input.
-        
+        System.out.println("Enter the mass :");
+        Scanner keyboard = new Scanner (System.in);
+        m = keyboard.nextDouble();
+        System.out.println("Enter the acceleration");
+        a = keyboard.nextDouble();
+        force=m*a;       
         // Show the result. (Example:"The value of the var is:" [*Unit measure include*])
+        System.out.println("The Force is :" + force + "m/s^2");
     }
     
 }
